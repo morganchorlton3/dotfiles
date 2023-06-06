4621871f7f784a5e4450856b9db4e042dc6aec55
Morgan Chorlton <morganchorlton3@gmail.com>
2023-06-06 09:17:46 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
+# Hide "last login" line when starting a new terminal session
+touch $HOME/.hushlogin
+
+# Install zsh
+echo 'Install oh-my-zsh'
+echo '-----------------'
+rm -rf $HOME/.oh-my-zsh
+curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Add global gitignore
 ln -s $HOME/.dotfiles/shell/.global-gitignore $HOME/.global-gitignore
 git config --global core.excludesfile $HOME/.global-gitignore
+
+# Symlink zsh prefs
+rm $HOME/.zshrc
+ln -s $HOME/.dotfiles/shell/.zshrc $HOME/.zshrc
+
+
+# Activate zsh
+cd ~/.dotfiles/shell
+chmod +x z.sh
+
+# WSL Setup for 1 password ssh keys
+# See (https://dev.to/d4vsanchez/use-1password-ssh-agent-in-wsl-2j6m)
+sudo apt install socat
+
+source ~/.dotfiles/shell/wsl/.agent-bridge.sh
