f8b98b8d2aee993d0d6ccc6003cadc228421a13a
Morgan Chorlton <morganchorlton3@gmail.com>
2024-12-17 11:19:26 +0000

    added docker install

--- install.sh
+++ install.sh
@@ -109,6 +109,27 @@ if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
   sudo apt update
   sudo apt-get install terraform
 
+  print_colored_text "Installing Docker" "bold_blue" "yes"
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  sudo usermod -aG docker $USER
+  newgrp docker
+
+  # Add the repository to Apt sources:
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  print_colored_text "Installing brew" "bold_blue" "yes"
+  NONINTERACTIVE=1 DEBIAN_FRONTEND=noninteractive /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  
+  print_colored_text "Installing Lazydocker" "bold_blue" "yes"
+  brew install jesseduffield/lazydocker/lazydocker
+
 else
     echo "Not Instaling software"
 fi
